acdbcf3eb113889a619f0522ad9b887d897ed40e
seong-ho-park <psh30095@gmail.com>
2021-02-14 22:42:52 +0900

    21.02.14 add subject1_true

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,7 @@ var fs = require('fs');
 /* GET home page. */
 router.get('/', function (req, res) {
   console.log('/', req.session);
-  fs.readFile('views/index.html', function (err, html) {
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.end(html);
-  });
+  res.redirect('https://psh4607.com/');
 });
 router.get('/portfolio/true', function (req, res) {
   console.log('/', req.session);
